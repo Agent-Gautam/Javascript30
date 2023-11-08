f62c1990311e686f1b5329ee1f31770a0e7d6857
Gautam <gautamanand931@gmail.com>
2023-11-08 23:08:13 +0530

    piano-kit done

--- Day1/main.js
+++ Day1/main.js
@@ -1,41 +1,40 @@
 const keyCount = 9;
-const keylabels = ['A','S','D','F','G','H','J','K','L'];
+const keylabels = ["A", "S", "D", "F", "G", "H", "J", "K", "L"];
 let board = document.querySelector("#piano-board");
 let audio = document.querySelector("audio");
 
 class Key {
-    
-    constructor(index) {
-        let keyname = "key" + (index+1);
-        let keysound = "./sounds/" + keyname +".mp3";
+  constructor(index) {
+    let keyname = "key" + (index + 1);
+    let keysound = "./sound/" + keyname + ".wav";
 
-        //defining a key 
-        let key = document.createElement("div");
-        key.classList.add("key");
-        board.append(key);
-        
+    //defining a key
+    let key = document.createElement("div");
+    key.classList.add("key");
+    board.append(key);
 
-        // for text inside a key
-        let keytext = keylabels[index];
-        key.innerText = keytext;
-        
+    // for text inside a key
+    let keytext = keylabels[index];
+    key.innerText = keytext;
 
-        //sound
-        window.addEventListener("keydown", function(event){
-            // console.log(typeof event.key);
-            let eventkey = event.key.toUpperCase();
-            if(eventkey == keytext){
-                audio.src = keysound;
-                audio.currentTime = 0;
-                audio.play();
-            }
-        }
-        )
-        return key;
-    }
+    //sound
+    window.addEventListener("keydown", function (event) {
+      let eventkey = event.key.toUpperCase();
+      if (eventkey == keytext) {
+        key.classList.add("playing-key");
+        audio.src = keysound;
+        audio.currentTime = 0;
+        audio.play();
+      }
+    });
+    key.addEventListener("transitionend", function () {
+      key.classList.remove("playing-key");
+    });
+    return key;
+  }
 }
 
 //making keys
 for (let i = 0; i < keyCount; i++) {
-    new Key(i);
+  new Key(i);
 }
