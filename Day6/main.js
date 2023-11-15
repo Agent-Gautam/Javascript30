1b29c4e58af7f34eae7239b42d0ca564038cc8e7
Gautam <gautamanand931@gmail.com>
2023-11-15 08:55:22 +0530

    image feature added

--- Day6/main.js
+++ Day6/main.js
@@ -2,32 +2,50 @@ let results = document.querySelector(".results");
 let image = document.querySelector(".image");
 let input = document.querySelector(".coninp");
 
-let endpoint = "https://gist.githubusercontent.com/pratikbutani/20ded7151103bb30737e2ab1b336eb02/raw/be1391e25487ded4179b5f1c8eedb81b01226216/country-flag.json";
+let endpoint =
+  "https://gist.githubusercontent.com/pratikbutani/20ded7151103bb30737e2ab1b336eb02/raw/be1391e25487ded4179b5f1c8eedb81b01226216/country-flag.json";
 const countriedobj = [];
 
 fetch(endpoint)
-    .then(response => response.json())
-    .then(data => {countriedobj.push(...data);
-    });
+  .then((response) => response.json())
+  .then((data) => {
+    countriedobj.push(...data);
+  })
+  .catch(error=>results.innerHTML = `<li>${error}</li>`);
 
-function findcountries(find){
-    let countryname = countriedobj.map(con=>con["country"]);
-    let regex = new RegExp(find,"gi");
-    let filtered = countryname.filter(con=>con.match(regex));
-    return filtered;
+function findcountries(find) {
+  let regex = new RegExp(find, "gi");
+  let filtered = countriedobj.filter((con) => con["country"].match(regex));
+  return filtered;
 }
-function display(){
-    let filcon = findcountries(this.value);
-    // console.log(filcon);
-    let pushstr = ""
-    for(let res of filcon){
-        pushstr += `<li>${res}</li>`
+function display(event) {
+  let inputValue = event.target.value.trim(); // Trim to handle whitespace-only input
+  removeall(); // Always clear the results first
+
+  if (inputValue !== "") {
+    let filcon = findcountries(inputValue);
+    for (let res of filcon) {
+      let li = document.createElement("li");
+      li.innerHTML = res["country"];
+      highlight(res["country"],inputValue);
+      results.append(li);
+      li.addEventListener("click", () => {
+        removeall();
+        input.value = res["country"];
+        let src = res["flag"];
+        image.src = src;
+      });
     }
-    results.innerHTML = pushstr;
+  }
 }
-function flag(event){
-    let filcon = findcountries()
-    // event.target.style.backgroundImage = `url(${})`
+function highlight(searchresult,input){
+  let replace = searchresult.replace(input,`<span class="highlight">${input}</span>`)
+  return replace
 }
-input.addEventListener("input",display);
-results.addEventListener("click",flag);
+function removeall() {
+    image.src = "";
+  while (results.firstChild) {
+    results.removeChild(results.firstChild);
+  }
+}
+input.addEventListener("input", display);
